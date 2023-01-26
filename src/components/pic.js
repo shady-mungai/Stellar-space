5394fc51fc8a16255053579f83ca5e492097090c
shady-mungai <shadrackmungai10@gmail.com>
2023-01-26 17:53:46 +0300

    Added api_key to fetch

--- src/components/pic.js
+++ src/components/pic.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 
  function DayPic(){
+
+const api_key = process.env.REACT_APP_API_KEY;
 
     const [pictureOfTheDay, setpictureOfTheDay] = useState([]);
 
    
         useEffect(()=>{
-            fetch('https://api.nasa.gov/planetary/apod?api_key=lavrMO5ACfh2RwMlUFPcKPCGJIlB1hbdokxfmiZU')
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
             .then(r => r.json())
             .then(data => setpictureOfTheDay(data))
+            
          },[])
-     console.log(data)
+     console.log(pictureOfTheDay)
     
 
    
