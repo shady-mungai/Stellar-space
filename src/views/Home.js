3e742876a5b10f266874368dc17b09e0b2cfda30
shady-mungai <shadrackmungai10@gmail.com>
2023-01-26 23:30:20 +0300

    Added dependancy to useEffect

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Home = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
   
-    const handleClick = () => {
-      setIsOpen(!isOpen);
-    };
+
         return (
           <div className='relative h-[480px]'>
             <div className='absolute z-20 flex h-full flex-col items-start justify-center space-y-4 ml-12 tex-white font-bold w-[800px]'>
               <h2 className='text-6xl'>Welcome to Stellar Space</h2>
               <p>Know what surrounds you.</p>
+              <div>
+              <p>Welcome to the NASA Home Page! This application aims to provide users with a wealth of information 
+                about NASA and its various missions, projects, and discoveries. From exploring the depths of space to studying the Earth and our solar system,
+                 NASA's mission is to drive advances in science, technology, aeronautics, and space exploration to enhance knowledge, education, innovation, economic vitality, and stewardship of Earth.</p>
+             </div>
+
+                
+              
              <div id='day'>
-             <div  className='bg-pink-500 px-[12px] py-[9px] text-[15px] font-bold text-white rounded-md hover:bg-pink-400'>
-              <a href="#pic"  onClick={handleClick}><i ></i> picture of the day </a>
-             </div> 
              </div>
             </div>
-            <div className='bg-black absolute opacity-70 w-full h-full'>
+            <div className=' absolute opacity-70 w-full h-full'>
 
             </div>
           </div>
