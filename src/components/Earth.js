4900126702abedbdd72f30f38a35010c3b53b2f6
maqiie <99504010+maqiie@users.noreply.github.com>
2023-01-26 12:22:38 +0300

    added styles

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -2,36 +2,42 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ObjectsNearEarth from "./ObjectsNearEarth";
 
+function Earth() {
+  const [data, setData] = useState([{}]);
 
- function Earth(){
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=YT2v2f6KZQXdSMtzPMrdu27ojErRY7iIsKHjn1KK"
+      )
+      .then((response) => {
+        setData(response.data.near_earth_objects);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(data[0]);
+  //  console.log(data[0].close_approach_data)
+  //  console.log(data)
 
-    const [data, setData] = useState([{}]);
+  // console.log(data[0][close_approach_data])
 
+  const eachObject = data.map((object, index) => {
+    return (
+      <div key={index}>
+        {" "}
+        <ObjectsNearEarth object={object} />{" "}
+      </div>
+    );
    
+      
 
-    useEffect(() => {
+  });
 
-        axios.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=YT2v2f6KZQXdSMtzPMrdu27ojErRY7iIsKHjn1KK')
-        .then((response)=>{
-            setData(response.data.near_earth_objects);
+  return eachObject;
+}
 
-        })
-        .catch((error)=>{
-            console.log(error);
-        });
-    },[]); 
-    console.log(data[0])
-     console.log(data[0].close_approach_data)
-    //  console.log(data)
-
-    // console.log(data[0][close_approach_data])
-
-const eachObject = data.map((object,index)=>{
-    return (<div key={index}> <ObjectsNearEarth object={object}/> </div>)
-        })
-
-return eachObject
-    
-   
- }
- export default Earth;
+ 
+export default Earth;
--- src/components/ObjectsNearEarth.js
+++ src/components/ObjectsNearEarth.js
@@ -1,25 +1,33 @@
 import React from "react";
 
-
- 
- function ObjectsNearEarth({object}) {
+function ObjectsNearEarth({ object , diameter }) {
   // console.log(data)
-  return(
-    <div className="object-container">
-      <p>{object.name}</p>
-      <p>{object.is_potentially_hazardous_asteroid
-}</p>
-        <p>{object.absolute_magnitude_h}</p>
-        {/* <p>{object.close_approach_data[0].close_approach_date}</p> */}
+  // console.log(object.nasa_jpl_url)
+  
+  return (
+    <div className="object-container   border border-solid border-red-400 
+    rounded-md p-4  bg-gray-400   hover:bg-green-100 text-center 
+      justify-center pt-5 mt-4 mb-4 ml-10 mr-10 shadow-lg">
+       
+        <ul  className="ml-4 box-border ">
+        <li className="text-3xl font-bold underline">NAME: {object.name}</li>
 
-    
-      {/* <img src={object.nasa_jpl_url} alt="nasa"/> */}
+        <li> LIMITED NAME: {object.name_limited}</li>
+        <li> MAGNITUDE:{object.absolute_magnitude_h}</li>
+        <li className="text-red-500">LINK: {object.nasa_jpl_url}</li>
+        <li>ID: {object.id}</li>
+        <li>
+          {/* 
+           */}
+           <li className={object.is_potentially_hazardous_asteroid ?
+             'text-red-500' : 'text-green-500'}>Hazardous:
+              {object.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</li>
+        </li>
+        <li>SENTRY: {object.is_sentry_object ? "Yes" : "No"}</li>
+      </ul>
     </div>
-  )
-
-  
-  }
+  );
+}
 
 
-  
 export default ObjectsNearEarth;
