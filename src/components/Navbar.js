40ae83722c41b18d85c5d3adc1450d9d03e4db2a
shady-mungai <shadrackmungai10@gmail.com>
2023-01-26 23:31:55 +0300

    added style to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,25 +6,26 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="bg-gray-700 text-white">
+    <div className="bg-gray-700 text-white h-20">
       <div className="flex justify-between items-center w-full h-19 container  mx-auto">
         {/* logo */}
         <div>
-          <h2 className="text-2xl font-bold">
+          <h2 className="text-3xl font-bold">
             <span>Stellar Space</span>
           </h2>
         </div>
         {/* three links -> home, analytics & profile */}
-        <div className="flex text-white gap-4 text-lg">
+        <div className="flex text-white gap-5 text-lg">
         <div>
-            <Link to="./pic">Pic Of The Day</Link>
+            <Link to="/">Home</Link>
+          </div>
+        <div>
+            <Link to="./DayPic">Pic Of The Day</Link>
           </div>
           <div>
             <Link to="/earth">Objects Near Earth</Link>
           </div>
-        <div>
-            <Link to="/">Home</Link>
-          </div>
+
           <div>
             <Link to="./Mars">Mars</Link>
           </div>
