6b6f8905e4b47aba03a203ee0e010291eca7c66e
shady-mungai <shadrackmungai10@gmail.com>
2023-01-26 12:20:45 +0300

    Passed down information to displayPhotos component

--- src/components/Mars.js
+++ src/components/Mars.js
@@ -1,44 +1,66 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import DisplayMarsPhotos from "./DisplayMarsPhotos";
-import MarsWeather from "./MarsWeather";
 
 const api_key = process.env.REACT_APP_API_KEY;
 
 function Mars() {
-    // state of the roverPhotos and its data
+  // state of the roverPhotos and its data
   const [roverPhotos, setRoverPhotos] = useState([{}]);
   useEffect(() => {
     axios
       .get(
         `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`
       )
-      .then((res) => setRoverPhotos(res.data.photos));
+      .then((res) => {
+                        setRoverPhotos(res.data.photos)
+                      });
   }, []);
-//  console.log(roverPhotos);
+   
+  console.log(roverPhotos[0])
 
   const eachImage = roverPhotos.map((roverPhoto, index) => {
     return (
-      <div key={index} className="bg-gray-200 object-contain grid">
-        <DisplayMarsPhotos roverPhoto={roverPhoto} camera={roverPhoto.camera}/>
-      </div>
+      <DisplayMarsPhotos
+        key={index}
+        roverPhoto={roverPhoto}
+      />
+  
     );
   });
-
- //state that contains initial Mars weather data
-const [marsWeather,setMarsWeather] = useState({})
-useEffect(()=>{
-    axios
-    .get(`https://api.nasa.gov/insight_weather/?api_key=${api_key}&feedtype=json&ver=1.0`)
-     .then(res=>setMarsWeather(res.data.validity_checks['1219']))
-},[])
-
   
 
-  return <div>
-  {roverPhotos ? <div>{eachImage}</div> : "Loading ..."}
-  <MarsWeather />
-  </div>;
+
+  //state that contains initial Mars weather data
+  const [marsTemp, setMarsTemp] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/insight_weather/?api_key=${api_key}&feedtype=json&ver=1.0`
+      )
+      .then((res) => {
+        setMarsTemp(res.data.validity_checks["1219"].AT.sol_hours_with_data);
+      });
+  }, []);
+
+  
+console.log(marsTemp)
+
+
+  return (
+    <div className="bg-gray-300 ">
+      {roverPhotos ? (
+        <div className="bg-gray-300 grid grid-cols-4">
+          {eachImage}
+          </div>
+      ) : (
+        "Loading ..."
+      )}
+      <br />
+
+    </div>
+  );
 }
 
 export default Mars;
