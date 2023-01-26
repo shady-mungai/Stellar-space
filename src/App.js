583bcb066a3a33cc81361c2bcebe46cf06b876de
nick <mwauranicholas10@gmail.com>
2023-01-26 12:03:07 +0300

    changed pic.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './index.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-
+import DayPic from './components/pic';
 
 function App() {
   return(
     <div>
       <Navbar />
       <Home />
+      <DayPic/>
     </div>
   )
 }
--- src/components/pic.js
+++ src/components/pic.js
@@ -4,23 +4,16 @@ import axios from "axios";
 
  function DayPic(){
 
-    const [data, setData] = useState([{}]);
+    const [pictureOfTheDay, setpictureOfTheDay] = useState([]);
 
    
-
-    useEffect(() => {
-
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=lavrMO5ACfh2RwMlUFPcKPCGJIlB1hbdokxfmiZU')
-        .then((response)=>{
-            setData(response.data.pic);
-
-        })
-        .catch((error)=>{
-            console.log(error);
-        });
-    },[]); 
-    console.log(data[0])
-    console.log(data[0].nasa_jpl_url)
+        useEffect(()=>{
+            fetch('https://api.nasa.gov/planetary/apod?api_key=lavrMO5ACfh2RwMlUFPcKPCGJIlB1hbdokxfmiZU')
+            .then(r => r.json())
+            .then(data => setpictureOfTheDay(data))
+         },[])
+     console.log(data)
+    
 
    
  }
