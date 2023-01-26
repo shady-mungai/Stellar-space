363c918b3c8c9ec2c540ef86064354e1c6ce8bae
shady-mungai <shadrackmungai10@gmail.com>
2023-01-26 12:50:57 +0300

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Mars from "./components/Mars";
+import Navbar from "./components/Navbar";
+import MarsWeather from "./components/MarsWeather";
+import Home from "./views/Home";
+
 
 function App() {
-  return <Mars />;
+  return (
+<div>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+
+    <Route path="/Mars" element={<Mars/>} />
+    <Route path="/MarsWeather" element={<MarsWeather/>} />
+
+  </Routes>
+</div>
+  );
 }
 export default App;
